c4dde39a0e81e1a147a01f695d594c075e7efcdd
Ray Brown-Amory <r.brown-amory@sky.com>
2022-07-04 19:26:27 +0100

    Add project files.

---
+++ RoomBookingApp.Coe.Test/BookingTest/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+
+namespace RoomBookingApp.Core.Test.BookingTest
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Reqesponse_with_Request_Values()
+        {
+            var bookingRequest = new RoomBookingRequest
+            {
+                FullName = "Test-Name",
+                Email  ="Test@gmail.com",
+                Date = new DateTime(2022, 6,4)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            RoomBookingResult result = processor.BookRoom(bookingRequest);
+
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(result.FullName);
+            result.Email.ShouldBe(result.Email);
+            result.Date.ShouldBe(result.Date);
+
+        }
+    }
+}
---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,21 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        public  RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            return new RoomBookingResult
+            {
+                FullName = bookingRequest.FullName,
+                Date = bookingRequest.Date,
+                Email = bookingRequest.Email,
+            };
+        }
+    }
+}
