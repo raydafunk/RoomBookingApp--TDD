68cc6618cfd81d3ef1dd6a797a85d802dddf1a1e
Ray Brown-Amory <r.brown-amory@sky.com>
2022-07-04 20:38:07 +0100

    Added Services and a Interface

---
+++ RoomBookingApp.Core/Domain/RoomBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace RoomBookingApp.Core.Domain
+{
+    public class RoomBooking
+    {
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ RoomBookingApp.Core/Services/IRoomBookingService.cs
@@ -0,0 +1,9 @@
+﻿using RoomBookingApp.Core.Domain;
+
+namespace RoomBookingApp.Core.Services
+{
+    public interface IRoomBookingService
+    {
+        void Save(RoomBooking roomBooking);
+    }
+}
